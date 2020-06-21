619c9ca7ca475bdced1f7a81f800d6258a2fdb3e
Momeemt <momiimt@gmail.com>
2020-06-21 10:39:37 +0900

    ✨ change theme

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,36 +1,72 @@
-// See more in https://github.com/ktquez/vuepress-theme-ktquez#configuration
-const path = require('path')
-const head = require('./config/head')
-const themeConfig = require('./config/themeConfig')
-const resolve = pathName => path.join(__dirname, pathName)
-
 module.exports = {
-  theme: 'ktquez',
-  head,
-  themeConfig,
-  base: '/',
-  title: `MY TITLE`,  
-  ga: `MY TRACKING CODE GOOGLE ANALYTICS`,
-  evergreen: true,  // For modern browsers
-  serviceWorker: true,
+  title: 'Momeemt Blog',
+  description: 'momeemt\'s technology blog',
   locales: {
     '/': {
-      lang: 'en'
+      lang: 'en-US',
     },
-    '/pt/': {
-      lang: 'pt-br'
-    },
-    '/es/': {
-      lang: 'es'
-    }
   },
-  configureWebpack () {
-    return {
-      resolve: {
-        alias: {
-          '@public': resolve('./public')
+  theme: 'meteorlxy',
+  themeConfig: {
+    lang: 'en-US',
+    personalInfo: {
+      nickname: 'Momiyama',
+      description: 'Rapid progress',
+      email: 'momiimt@gmail.com',
+      location: 'Tokyo, Japan',
+      organization: 'Oshukan Secondary School',
+      avatar: 'https://pbs.twimg.com/profile_images/1127473315944132608/nRUcPCRj_400x400.jpg',
+      sns: {
+        github: {
+          account: 'momeemt',
+          link: 'https://github.com/momeemt',
+        },
+        twitter: {
+          account: 'momeemt',
+          link: 'https://twitter.com/momeemt',
+        },
+        instagram: {
+          account: 'momeemt',
+          link: 'https://www.instagram.com/momeemt',
         }
-      }
-    }
-  }
+      },
+    },
+    header: {
+      background: {
+        url: 'https://assets.st-note.com/production/uploads/images/17453940/e903703e20ba647b3af25d868e6d5a9b.jpg?crop=1.6%3A0.27&quality=60',
+        useGeo: true,
+      },
+      showTitle: false,
+    },
+
+    lastUpdated: true,
+    nav: [
+      { text: 'Home', link: '/', exact: true },
+      { text: 'Posts', link: '/posts/', exact: false },
+    ],
+
+    // Comments config. See the [Posts Comments] section below.
+    // comments: {
+    //   owner: 'meteorlxy',
+    //   repo: 'vuepress-theme-meteorlxy',
+    //   clientId: 'MY_CLIENT_ID',
+    //   clientSecret: 'MY_CLIENT_SECRET',
+    // },
+
+    // Pagination config
+    pagination: {
+      perPage: 20,
+    },
+
+    // Default Pages (Optional, the default value of all pages is `true`)
+    defaultPages: {
+      // Allow theme to add Home page (url: /)
+      home: true,
+      // Allow theme to add Posts page (url: /posts/)
+      posts: true,
+    },
+  },
+  head: [
+    ['link', { href: '/css/style.css', rel: 'stylesheet'}]
+  ]
 }
---
+++ src/_posts/first-post.md
@@ -0,0 +1,38 @@
+---
+title: 'I have created my blog! (and tree structure)'
+date: 2020-06-21
+category: diary
+tags:
+---
+# I have created my blog!
+Hi. I am Momiyama(@momeemt).  
+I decide on I write articles by English, because I want to talk English with flequency but I can't write in it well.  
+My hobby is programming, so I will post about information technology.  
+
+## About Tree Structure
+Today, I introduce tree structure.  
+It is data structure which can construct hirerarchical structure.  
+It has nodes and edges.  
+In the figure, node is circle, edge is line.
+
+### rooted tree
+If a tree structure has root which is distinguished node, it named rooted tree.  
+Worthy of a special mention, it has parent-child relationship.  
+Depth of root node is 0, and depth of nodes which are connecting root node is 1.  
+In this way, the relation that connecting and depth of nodes is 1 more than the node is parent-child relationship.  
+By the way, the relation that nodes are same depth as the node is sibling.  
+The node which does not have child-node is named 'external node' or 'leaf'.  
+
+## Impression
+Writing this article in English is very hard work, so I cannot make detailed information.  
+I am not good at English, but I want to do my best posting article everyday.
+
+## New Words
+| English | Japanese |
+| ------- | -------- |
+| construct | 構築する |
+| hierarchical | 階層的 |
+| rooted tree | 根付き木 |
+| distinguished | 区別された |
+| Worthy of a special mention | 特筆すべきなのは |
+| impression | 感想 |
